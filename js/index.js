7202f28001c3689c5ec247f2c1a22835579e83e7
Student <student@StudentiMac-33.local>
2022-07-29 13:43:02 -0400

    added showing and hiding of page links when the hamburger icon or cross icon are pressed on mobile

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ function gotToContactPage() {
 
 const hamburger = document.getElementById("hamburger");
 const cross = document.getElementById("cross");
+const pageLinks = document.getElementById("pageLinks");
 
 if(hamburger != undefined) {
     hamburger.style.display = "block";
@@ -19,13 +20,19 @@ if(cross != undefined) {
     cross.style.display = "none";
 }
 
+if(pageLinks != undefined) {
+    pageLinks.style.display = "none";
+}
+
 function showNavigationLinks() {
     if(cross.style.display == "none" && hamburger.style.display == "block") {
         cross.style.display = "block";
         hamburger.style.display = "none";
+        pageLinks.style.display = "block";
     } else {
         cross.style.display = "none";
         hamburger.style.display = "block";
+        pageLinks.style.display = "none";
     }
 }
 
@@ -33,8 +40,10 @@ function hideAll() {
     if(screen.width > 375) {
         cross.style.display = "none";
         hamburger.style.display = "none";
+        pageLinks.style.display = "block";
     } else if(screen.width == 375) {
         hamburger.style.display = "block";
+        pageLinks.style.display = "none";
     }
 }
 
