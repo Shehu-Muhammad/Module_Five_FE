d6cecd88147ea970f4948819d231b7004cd5265d
Student <student@StudentiMac-33.local>
2022-07-29 13:20:37 -0400

    added hamburger icon and cross icon for mobile links

--- js/index.js
+++ js/index.js
@@ -7,3 +7,37 @@ if(indexPageButton != undefined) {
 function gotToContactPage() {
     window.location.href = "contact.html";
 }
+
+const hamburger = document.getElementById("hamburger");
+const cross = document.getElementById("cross");
+
+if(hamburger != undefined) {
+    hamburger.style.display = "block";
+}
+
+if(cross != undefined) {
+    cross.style.display = "none";
+}
+
+function showNavigationLinks() {
+    if(cross.style.display == "none" && hamburger.style.display == "block") {
+        cross.style.display = "block";
+        hamburger.style.display = "none";
+    } else {
+        cross.style.display = "none";
+        hamburger.style.display = "block";
+    }
+}
+
+function hideAll() {
+    if(screen.width > 375) {
+        cross.style.display = "none";
+        hamburger.style.display = "none";
+    } else if(screen.width == 375) {
+        hamburger.style.display = "block";
+    }
+}
+
+window.addEventListener("resize", (e)=> {
+    hideAll();
+});
